d7881e54964ebdb0565c9b54e3df3ff9304501d6
golu12 <shivam.kesherwani@gmail.com>
2022-03-15 10:38:06 +0100

    Update JwtUtil.java

--- spring-security-jwt-master/src/main/java/io/javabrains/springsecurityjwt/util/JwtUtil.java
+++ spring-security-jwt-master/src/main/java/io/javabrains/springsecurityjwt/util/JwtUtil.java
@@ -16,18 +16,39 @@ public class JwtUtil {
 
     private String SECRET_KEY = "secret";
 
+    /**
+     * Method used for extractUsername by calling extractClaim method when we supply token.
+     * @param token
+     * @return
+     */
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
     }
 
+    /**
+     * Method used for extractExpiration by calling extractClaim method when we supply token.
+     * @param token
+     * @return
+     */
     public Date extractExpiration(String token) {
         return extractClaim(token, Claims::getExpiration);
     }
 
+    /**
+     * Method used for extract claims  by we supply token which will return all resolve all claim details of token.
+     * @param token
+     * @return
+     */
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
     }
+
+    /**
+     * Method used for extract claims  by we supply token.
+     * @param token
+     * @return
+     */
     private Claims extractAllClaims(String token) {
         return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
     }
@@ -36,11 +57,22 @@ public class JwtUtil {
         return extractExpiration(token).before(new Date());
     }
 
+    /**
+     * Below method is very important method which accept userDetails and call createToken.
+     * @param userDetails
+     * @return
+     */
     public String generateToken(UserDetails userDetails) {
         Map<String, Object> claims = new HashMap<>();
         return createToken(claims, userDetails.getUsername());
     }
 
+    /**
+     * Below method responsible for create Token by supplying some parameter.
+     * @param claims this contains some info for the token which we will use later while claim the username.
+     * @param subject which is a value that identifies the principal user. This value can be a user ID or userName for which this JWT was generated,
+     * @return
+     */
     private String createToken(Map<String, Object> claims, String subject) {
 
         return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
@@ -48,6 +80,11 @@ public class JwtUtil {
                 .signWith(SignatureAlgorithm.HS256, SECRET_KEY).compact();
     }
 
+    /**
+     * Method used for checking whether the token is expired claims  by we supply token.
+     * @param token
+     * @return
+     */
     public Boolean validateToken(String token, UserDetails userDetails) {
         final String username = extractUsername(token);
         return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
